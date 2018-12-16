38eee43d920c221fc84a3bac94bc0a7d6f7e0c5b
Andzej Maciusovic <andzej.maciusovic@gmail.com>
2018-12-16 17:43:53 +0200

    add middleware commons

--- nio_test.go
+++ nio_test.go
@@ -188,28 +188,6 @@ func TestNioWrapHandler(t *testing.T) {
 	}
 }
 
-func TestNioWrapMiddleware(t *testing.T) {
-	e := New()
-	req := httptest.NewRequest(http.MethodGet, "/", nil)
-	rec := httptest.NewRecorder()
-	c := e.NewContext(req, rec)
-	buf := new(bytes.Buffer)
-	mw := WrapMiddleware(func(h http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			buf.Write([]byte("mw"))
-			h.ServeHTTP(w, r)
-		})
-	})
-	h := mw(func(c Context) error {
-		return c.String(http.StatusOK, "OK")
-	})
-	if assert.NoError(t, h(c)) {
-		assert.Equal(t, "mw", buf.String())
-		assert.Equal(t, http.StatusOK, rec.Code)
-		assert.Equal(t, "OK", rec.Body.String())
-	}
-}
-
 func TestNioConnect(t *testing.T) {
 	e := New()
 	testMethod(t, http.MethodConnect, "/", e)
